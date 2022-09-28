8034f89f262c28d82a7cd5aed0277b9657612a4b
VOLODYMYR BASHTANYUK <96777965+VolodymyrBashtanyuk@users.noreply.github.com>
2022-09-28 10:20:20 +0300

    add hooks instead of classes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,62 +1,121 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Section } from './Section/Section';
 import { FeedbackOptions } from './FeedbackOption/FeedbackOption';
 import { Notification } from './Notification/Notification';
 import { Statistics } from './Statistic/Statistic';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export const App = () => {
+  const buttons = ['Good', 'Neutral', 'Bad'];
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedback = () => {
     return good + neutral + bad;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((good / countTotalFeedback()) * 100);
   };
 
-  onIncrementFeedbackStatistic = e => {
-    const buttonName = e.currentTarget.name;
-
-    this.setState(prevState => ({
-      [buttonName]: prevState[buttonName] + 1,
-    }));
+  const onIncrementFeedbackStatistic = e => {
+    const { name } = e.currentTarget;
+    switch (name) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        return;
+    }
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const buttons = ['Good', 'Neutral', 'Bad'];
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={buttons}
+          onLeaveFeedback={onIncrementFeedbackStatistic}
+        />
+      </Section>
 
-    return (
-      <>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={buttons}
-            onLeaveFeedback={this.onIncrementFeedbackStatistic}
-          />
-        </Section>
+      <Section title="Statistic">
+        <>
+          {countTotalFeedback() === 0 ? (
+            <Notification />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={countTotalFeedback()}
+              positivePercentage={countPositiveFeedbackPercentage()}
+            />
+          )}
+        </>
+      </Section>
+    </>
+  );
+};
 
-        <Section title="Statistic">
-          <>
-            {this.countTotalFeedback() === 0 ? (
-              <Notification />
-            ) : (
-              <Statistics
-                good={good}
-                neutral={neutral}
-                bad={bad}
-                total={this.countTotalFeedback()}
-                positivePercentage={this.countPositiveFeedbackPercentage()}
-              />
-            )}
-          </>
-        </Section>
-      </>
-    );
-  }
-}
+// export class OldApp extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+
+//   countTotalFeedback = () => {
+//     const { good, neutral, bad } = this.state;
+//     return good + neutral + bad;
+//   };
+
+//   countPositiveFeedbackPercentage = () => {
+//     return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+//   };
+
+//   onIncrementFeedbackStatistic = e => {
+//     const buttonName = e.currentTarget.name;
+
+//     this.setState(prevState => ({
+//       [buttonName]: prevState[buttonName] + 1,
+//     }));
+//   };
+
+//   render() {
+//     const { good, neutral, bad } = this.state;
+//     const buttons = ['Good', 'Neutral', 'Bad'];
+
+//     return (
+//       <>
+//         <Section title="Please leave feedback">
+//           <FeedbackOptions
+//             options={buttons}
+//             onLeaveFeedback={this.onIncrementFeedbackStatistic}
+//           />
+//         </Section>
+
+//         <Section title="Statistic">
+//           <>
+//             {this.countTotalFeedback() === 0 ? (
+//               <Notification />
+//             ) : (
+//               <Statistics
+//                 good={good}
+//                 neutral={neutral}
+//                 bad={bad}
+//                 total={this.countTotalFeedback()}
+//                 positivePercentage={this.countPositiveFeedbackPercentage()}
+//               />
+//             )}
+//           </>
+//         </Section>
+//       </>
+//     );
+//   }
+// }
